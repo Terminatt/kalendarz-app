01950258a030a0dfccd00243d7334a83114655d4
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2021-11-09 22:01:38 +0100

    cleanup

--- api/users/models.py
+++ api/users/models.py
@@ -1,9 +1,6 @@
-from django.contrib.auth.hashers import make_password
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser, Group, BaseUserManager
-
-
+from django.contrib.auth.models import AbstractUser, Group
 
 class User(AbstractUser):
     email = models.TextField(unique=True)
