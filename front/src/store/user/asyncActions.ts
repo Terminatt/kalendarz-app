6d9fa056eed005af2ddbcacd6bc46f028ad1c7a4
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2021-12-03 21:18:06 +0100

    redux added

---
+++ front/src/store/user/asyncActions.ts
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from "@reduxjs/toolkit"
+
+export const getCharacterMetadata = createAsyncThunk(
+  'creator/getCharacterMetadata',
+  async () => {
+    // const response = await axios.get<{ test: number }>(
+    //   'https://reqres.in/api/users',
+    // )
+    return null
+  },
+)
+
---
+++ front/src/store/user/reducer.ts
@@ -0,0 +1,14 @@
+import { createReducer } from "@reduxjs/toolkit"
+import { updateLoading } from "./actions"
+import { UserState } from "./types"
+
+const initialState: UserState = {
+  isLoading: false,
+}
+
+export const userReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(updateLoading, (state, action) => {
+      state.isLoading = action.payload.loading
+    })
+})
