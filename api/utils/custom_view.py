06e658425d856574981cb7ccaddad7e4793337ad
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2021-12-06 20:41:50 +0100

    refactor code

--- api/utils/custom_view.py
+++ api/utils/custom_view.py
@@ -10,22 +10,27 @@ class CustomModelViewSet(ModelViewSet):
     """
     acl_name = None
 
+    def import_permission_class(self, acl):
+        return getattr(importlib.import_module("users.permission"), "Is" + acl)
+    
+    def get_acl_json_list(self):
+        if self.action == None:
+            return []
+
+        return acl_matrix[self.acl_name]["permissions"][self.action]
+
     def get_permission_from_acl(self, acls):
         permissions = []
         for acl in acls:
-            PermissionClass = getattr(importlib.import_module("users.permission"), "Is" + acl)
+            PermissionClass = self.import_permission_class(acl=acl)
             permissions.append(PermissionClass)
         return permissions
 
     def get_permissions(self):
         if self.acl_name == None:
             return []
-
-        if self.action == None:
-            return []
         
-        acl_list = []
-        acl_list = acl_matrix[self.acl_name]["permissions"][self.action]
+        acl_list = self.get_acl_json_list()
 
         permission_classes = self.get_permission_from_acl(acls=acl_list)
         return [permission() for permission in permission_classes]
