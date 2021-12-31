c838114beeef8dcab13ced79baffb31f3ab603b4
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2021-12-31 17:15:30 +0100

    feat(api+front): add dynamic validation errors from backend

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
+from utils.response_error import ErrorType, ResponseError
 from utils.custom_validators import CustomValidation
 from users.models import User
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import Group
 from constants import GROUPS
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -24,9 +26,10 @@ class UserSerializer(serializers.ModelSerializer):
         CustomValidation().validate_email(email_field=email)
 
         if User.objects.filter(email__iexact=email).exists():
-            raise serializers.ValidationError("This email was arleady taken")
+            raise serializers.ValidationError(ResponseError.get_error_dict(errorType=ErrorType.EMAIL_TAKEN, msg='User with this email arleady exists'))
 
         return email
+
 
     def validate_password(self, pswd):
         CustomValidation().validate_password(password=pswd)
@@ -38,5 +41,10 @@ class UserSerializer(serializers.ModelSerializer):
                   'email', 'title', 'password', 'username', 'groups', 'created']
         extra_kwargs = {
             'password': {'write_only': True},
-            'groups': {'required': False}
-            }
+            'groups': {'required': False},
+            'username': {
+                'validators': [
+                    UniqueValidator(queryset=User.objects.all(), message=ResponseError.get_error_dict(errorType=ErrorType.USERNAME_TAKEN, msg='User with this username arleady exists'))
+                ]
+            },
+        }
