6b385b8089d5a47070aa05cfd9baa1387f06aa3b
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2022-01-10 12:01:50 +0100

    tests(front): remove jest workaround

--- front/src/utils/general.ts
+++ front/src/utils/general.ts
@@ -2,19 +2,6 @@ abstract class GeneralUtils {
     static isNumber(value: unknown): value is number {
         return typeof value === 'number';
     }
-
-    static jestWorkAround() {
-        window.matchMedia = (query) => ({
-            matches: false,
-            media: query,
-            onchange: null,
-            addListener: jest.fn(), // deprecated
-            removeListener: jest.fn(), // deprecated
-            addEventListener: jest.fn(),
-            removeEventListener: jest.fn(),
-            dispatchEvent: jest.fn(),
-        });
-    }
 }
 
 export default GeneralUtils;
