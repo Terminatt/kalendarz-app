c407d9dd72052f919bebab67944c97cb77ac9cf9
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-02-17 17:37:59 +0100

    feat(front): add rooms for reservation panel

--- front/src/components/ReservationPanel/ReservationPanel.tsx
+++ front/src/components/ReservationPanel/ReservationPanel.tsx
@@ -1,9 +1,12 @@
 import ColoredBlock from '@components/ColoredBlock/ColoredBlock';
 import HugeDivider from '@components/HugeDivider/HugeDivider';
 import { DAY_NAMES_FULL, WORKING_HOURS } from '@constants/constants';
+import { RootState } from '@store/index';
+import { getRooms } from '@store/rooms/asyncActions';
 import { joinClassNames } from '@utils/general';
 import { Dayjs } from 'dayjs';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './ReservationPanel.less';
 
@@ -14,6 +17,13 @@ export interface ReservationPanelProps {
 
 const ReservationPanel: React.FC<ReservationPanelProps> = (props) => {
     const { day, className } = props;
+    const dispatch = useDispatch();
+    const { data } = useSelector((state: RootState) => state.rooms);
+    const { results } = data;
+
+    useEffect(() => {
+        dispatch(getRooms());
+    }, []);
 
     return (
         <div className={joinClassNames(['reservation-panel', className])}>
@@ -27,6 +37,12 @@ const ReservationPanel: React.FC<ReservationPanelProps> = (props) => {
                         <ColoredBlock className="reservation-panel-content-header-block-left">Nazwa sali</ColoredBlock>
                         <ColoredBlock className="reservation-panel-content-header-block-left">Ilość miejsc</ColoredBlock>
                     </div>
+                    {results.map((el) => (
+                        <div key={el.id} className="reservation-panel-content-row">
+                            <ColoredBlock bgColor={el.type.color} className="reservation-panel-content-header-block-left">{el.name}</ColoredBlock>
+                            <ColoredBlock bgColor={el.type.color} className="reservation-panel-content-header-block-left">100</ColoredBlock>
+                        </div>
+                    ))}
                 </div>
                 <div className="reservation-panel-content-right">
                     <div className="reservation-panel-content-row reservation-panel-content-row-right">
