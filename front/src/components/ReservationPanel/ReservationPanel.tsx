552e37bcb04a02b1a5aa0fe8f5072f7519d6eaa4
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-02-17 17:41:28 +0100

    refactor(front): move api call to page

--- front/src/components/ReservationPanel/ReservationPanel.tsx
+++ front/src/components/ReservationPanel/ReservationPanel.tsx
@@ -1,29 +1,21 @@
 import ColoredBlock from '@components/ColoredBlock/ColoredBlock';
 import HugeDivider from '@components/HugeDivider/HugeDivider';
 import { DAY_NAMES_FULL, WORKING_HOURS } from '@constants/constants';
-import { RootState } from '@store/index';
-import { getRooms } from '@store/rooms/asyncActions';
+import { Room } from '@store/rooms/types';
 import { joinClassNames } from '@utils/general';
 import { Dayjs } from 'dayjs';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 
 import './ReservationPanel.less';
 
 export interface ReservationPanelProps {
     day: Dayjs;
     className?: string;
+    rooms: Room[]
 }
 
 const ReservationPanel: React.FC<ReservationPanelProps> = (props) => {
-    const { day, className } = props;
-    const dispatch = useDispatch();
-    const { data } = useSelector((state: RootState) => state.rooms);
-    const { results } = data;
-
-    useEffect(() => {
-        dispatch(getRooms());
-    }, []);
+    const { day, className, rooms } = props;
 
     return (
         <div className={joinClassNames(['reservation-panel', className])}>
@@ -37,7 +29,7 @@ const ReservationPanel: React.FC<ReservationPanelProps> = (props) => {
                         <ColoredBlock className="reservation-panel-content-header-block-left">Nazwa sali</ColoredBlock>
                         <ColoredBlock className="reservation-panel-content-header-block-left">Ilość miejsc</ColoredBlock>
                     </div>
-                    {results.map((el) => (
+                    {rooms.map((el) => (
                         <div key={el.id} className="reservation-panel-content-row">
                             <ColoredBlock bgColor={el.type.color} className="reservation-panel-content-header-block-left">{el.name}</ColoredBlock>
                             <ColoredBlock bgColor={el.type.color} className="reservation-panel-content-header-block-left">100</ColoredBlock>
--- front/src/pages/RoomReservation/RoomReservation.tsx
+++ front/src/pages/RoomReservation/RoomReservation.tsx
@@ -1,18 +1,31 @@
 import ReservationPanel from '@components/ReservationPanel/ReservationPanel';
 import useQuery from '@hooks/useQuery';
+import { RootState } from '@store/index';
+import { getRooms } from '@store/rooms/asyncActions';
 import dayjs from 'dayjs';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './RoomReservation.less';
 
 const RoomReservation: React.FC = () => {
     const query = useQuery();
     const day = dayjs(query.get('day'));
+    const { data } = useSelector((state: RootState) => state.rooms);
+    const { results } = data;
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getRooms());
+    }, []);
 
     return (
         <div className="room-reservation">
             <div className="room-reservation-content">
-                <ReservationPanel day={day} />
+                <ReservationPanel
+                    rooms={results}
+                    day={day}
+                />
             </div>
         </div>
     );
