d76a9460903638695dd0d2dbd5f74f0d065004ee
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-02-19 14:00:14 +0100

    feat(front): add tooltip overlay

--- front/src/components/TimeBlock/TimeBlock.tsx
+++ front/src/components/TimeBlock/TimeBlock.tsx
@@ -1,4 +1,6 @@
+import { GenericReactContent } from '@generics/generics';
 import { joinClassNames } from '@utils/general';
+import { Tooltip } from 'antd';
 import React from 'react';
 
 import './TimeBlock.less';
@@ -6,18 +8,31 @@ import './TimeBlock.less';
 export interface TimeBlockProps {
     className?: string;
     ariaLabel?: string;
+    tooltipOverlay?: GenericReactContent;
     selected?: boolean;
 }
 
 const TimeBlock: React.FC<TimeBlockProps> = (props) => {
-    const { className, ariaLabel, selected } = props;
+    const {
+        className, ariaLabel, selected, tooltipOverlay,
+    } = props;
 
-    return (
+    const renderBlock = () => (
         <button
             type="button"
             aria-label={ariaLabel}
             className={joinClassNames(['time-block', selected ? 'time-block-selected' : null, className])}
         />
+    );
+
+    return (
+        <>
+            {tooltipOverlay ? (
+                <Tooltip overlay={tooltipOverlay}>
+                    {renderBlock()}
+                </Tooltip>
+            ) : renderBlock()}
+        </>
     );
 };
 
