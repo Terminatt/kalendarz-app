b201bdf4f4c05b454d710d15495e11a7fc2b74e0
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-02-20 17:02:52 +0100

    feat(front): add summary below reservation panel

--- front/src/components/ReservationPanel/ReservationPanel.tsx
+++ front/src/components/ReservationPanel/ReservationPanel.tsx
@@ -4,10 +4,12 @@ import TimeBlock from '@components/TimeBlock/TimeBlock';
 import { DAY_NAMES_FULL, WORKING_HOURS } from '@constants/constants';
 import { Id } from '@generics/generics';
 import { Room } from '@store/rooms/types';
-import { joinClassNames } from '@utils/general';
+import { getEntries, joinClassNames } from '@utils/general';
 import { Dayjs } from 'dayjs';
 import React, { useCallback, useState } from 'react';
 import cloneDeep from 'lodash.clonedeep';
+import CustomButton from '@components/CustomButton/CustomButton';
+import { DeleteOutlined } from '@ant-design/icons';
 import { getTimeBlockValue, isTimeBlockSelected } from './helpers';
 
 import './ReservationPanel.less';
@@ -27,6 +29,7 @@ export interface TimeInterval {
     startDisplayValue: string | null;
     end: number | null;
     endDisplayValue: string | null;
+    room: Room;
 }
 
 export interface HoveredFocusedBlocksHashMap {
@@ -46,31 +49,29 @@ const ReservationPanel: React.FC<ReservationPanelProps> = (props) => {
     } = props;
     const [selectedBlocks, setSelectedBlocks] = useState<SelectedBlocksHashMap>({});
     const [hoveredFocusedBlocks, setHoveredFocusedBlock] = useState<HoveredFocusedBlocksHashMap>({});
+    const selectedBlockEntries = getEntries(selectedBlocks);
 
-    const onTimeBlockClick = useCallback((roomId: Id, timeBlock: number, displayValue: string): void => {
+    const onTimeBlockClick = useCallback((room: Room, timeBlock: number, displayValue: string): void => {
+        const { id } = room;
         const blockCopy = cloneDeep(selectedBlocks);
-        if (!blockCopy[roomId]) {
-            blockCopy[roomId] = {
+        if (!blockCopy[id]) {
+            blockCopy[id] = {
                 start: null,
                 end: null,
                 startDisplayValue: null,
                 endDisplayValue: null,
+                room,
             };
         }
 
-        if (!blockCopy[roomId].start) {
-            blockCopy[roomId].start = timeBlock;
-            blockCopy[roomId].startDisplayValue = displayValue;
-        } else if (blockCopy[roomId].start === timeBlock) {
-            blockCopy[roomId] = {
-                start: null,
-                startDisplayValue: null,
-                end: null,
-                endDisplayValue: null,
-            };
+        if (!blockCopy[id].start) {
+            blockCopy[id].start = timeBlock;
+            blockCopy[id].startDisplayValue = displayValue;
+        } else if (blockCopy[id].start === timeBlock) {
+            delete blockCopy[id];
         } else {
-            blockCopy[roomId].end = timeBlock;
-            blockCopy[roomId].endDisplayValue = displayValue;
+            blockCopy[id].end = timeBlock;
+            blockCopy[id].endDisplayValue = displayValue;
         }
 
         setSelectedBlocks(blockCopy);
@@ -101,21 +102,22 @@ const ReservationPanel: React.FC<ReservationPanelProps> = (props) => {
         setHoveredFocusedBlock(hoveredFocusedCopy);
     }, [hoveredFocusedBlocks]);
 
-    const renderTimeBlocks = useCallback((workingHour: number, roomId: Id) => timeBlockPerHour.map((timeElement) => {
+    const renderTimeBlocks = useCallback((workingHour: number, room: Room) => timeBlockPerHour.map((timeElement) => {
+        const { id } = room;
         const minutes = getTimeBlockValue(timeElement);
         const timeValue = parseInt(`${workingHour}${minutes}`, 10);
-        const block = selectedBlocks[roomId];
-        const hoveredFocused = hoveredFocusedBlocks[roomId];
+        const block = selectedBlocks[id];
+        const hoveredFocused = hoveredFocusedBlocks[id];
         const textValue = `${workingHour}:${minutes}`;
 
         return (
             <TimeBlock
                 selected={isTimeBlockSelected(timeValue, block, hoveredFocused)}
-                onMouseOver={() => onTimeBlockHover(timeValue, roomId, 'hovered')}
-                onFocus={() => onTimeBlockHover(timeValue, roomId, 'focused')}
-                onMouseLeave={() => onTimeBlockHoverOut(roomId, 'hovered')}
-                onBlur={() => onTimeBlockHoverOut(roomId, 'focused')}
-                onClick={() => onTimeBlockClick(roomId, timeValue, textValue)}
+                onMouseOver={() => onTimeBlockHover(timeValue, id, 'hovered')}
+                onFocus={() => onTimeBlockHover(timeValue, id, 'focused')}
+                onMouseLeave={() => onTimeBlockHoverOut(id, 'hovered')}
+                onBlur={() => onTimeBlockHoverOut(id, 'focused')}
+                onClick={() => onTimeBlockClick(room, timeValue, textValue)}
                 tooltipOverlay={`${block?.start !== timeValue && block?.startDisplayValue ? `${block.startDisplayValue} - ` : ''}${textValue}`}
                 key={`${workingHour}${timeElement}`}
                 aria-label={workingHour}
@@ -123,7 +125,6 @@ const ReservationPanel: React.FC<ReservationPanelProps> = (props) => {
             />
         );
     }), [selectedBlocks, hoveredFocusedBlocks, onTimeBlockClick]);
-
     return (
         <div className={joinClassNames(['reservation-panel', className])}>
             <HugeDivider className="reservation-panel-divider" text={DAY_NAMES_FULL[day.day()]} />
@@ -152,11 +153,40 @@ const ReservationPanel: React.FC<ReservationPanelProps> = (props) => {
                         </div>
                         {rooms.map((room) => (
                             <div key={room.id} className="reservation-panel-container-content-row reservation-panel-container-content-row-right">
-                                {WORKING_HOURS.map((el) => renderTimeBlocks(el, room.id))}
+                                {WORKING_HOURS.map((el) => renderTimeBlocks(el, room))}
                             </div>
                         ))}
                     </div>
                 </div>
+                {selectedBlockEntries.length !== 0 ? (
+                    <div className="reservation-panel-container-summary">
+                        <h2>Podsumowanie</h2>
+                        {selectedBlockEntries.map(([roomId, interval]) => (
+                            <div key={roomId} className="reservation-panel-container-summary-item">
+                                <CustomButton icon={<DeleteOutlined />} size="small" variant="delete">
+                                    Usuń
+                                </CustomButton>
+                                <div className="reservation-panel-container-summary-item-details">
+                                    {interval.room.name}
+                                    , 100 miejsc,&nbsp;
+                                    {interval.startDisplayValue}
+                                    &nbsp;
+                                    -
+                                    &nbsp;
+                                    {interval.endDisplayValue ? interval.endDisplayValue : 'Brak'}
+                                </div>
+                            </div>
+                        ))}
+                        <div className="reservation-panel-container-summary-action">
+                            <CustomButton className="reservation-panel-container-summary-action-btn">
+                                Zarezerwuj sale
+                            </CustomButton>
+                            <CustomButton className="reservation-panel-container-summary-action-btn" variant="clear">
+                                Wyczyść
+                            </CustomButton>
+                        </div>
+                    </div>
+                ) : null}
             </div>
         </div>
     );
