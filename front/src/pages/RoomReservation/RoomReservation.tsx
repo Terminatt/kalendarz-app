42e36ad830355080fa5cbb8c0307e06ce0fc3393
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-02-28 17:32:07 +0100

    feat(front): add changing of day in reservation panel

--- front/src/pages/RoomReservation/RoomReservation.tsx
+++ front/src/pages/RoomReservation/RoomReservation.tsx
@@ -2,14 +2,17 @@ import ReservationPanel from '@components/ReservationPanel/ReservationPanel';
 import useQuery from '@hooks/useQuery';
 import { RootState } from '@store/index';
 import { getRooms } from '@store/rooms/asyncActions';
+import { parseIsoDate } from '@utils/general';
 import dayjs from 'dayjs';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 
 import './RoomReservation.less';
 
 const RoomReservation: React.FC = () => {
     const query = useQuery();
+    const navigate = useNavigate();
     const day = dayjs(query.get('day'));
     const { data } = useSelector((state: RootState) => state.rooms);
     const { results } = data;
@@ -19,6 +22,11 @@ const RoomReservation: React.FC = () => {
         dispatch(getRooms());
     }, []);
 
+    const onArrowClick = useCallback((direction: 'left' | 'right') => {
+        const isoDate = parseIsoDate(day.add(direction === 'left' ? -1 : 1, 'day'));
+        navigate(`/room-reservation?day=${isoDate}`);
+    }, [day]);
+
     return (
         <div className="room-reservation">
             <div className="room-reservation-content">
@@ -26,6 +34,8 @@ const RoomReservation: React.FC = () => {
                     timeBlockContainerClassname="room-reservation-content-timeblocks"
                     rooms={results}
                     day={day}
+                    onLeftSwitcherClick={() => onArrowClick('left')}
+                    onRightSwitcherClick={() => onArrowClick('right')}
                 />
             </div>
         </div>
