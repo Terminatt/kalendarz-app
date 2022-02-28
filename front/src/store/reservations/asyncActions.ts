c4f4480e1e654d7c4ed9aa713a1c29d1ec9379ee
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-02-28 17:56:46 +0100

    feat(front): change get to getList for reservations

--- front/src/store/reservations/asyncActions.ts
+++ front/src/store/reservations/asyncActions.ts
@@ -1,16 +1,17 @@
-import { Id } from '@generics/generics';
+import { Id, ListRequestPayload } from '@generics/generics';
+import { getList } from '@utils/requests';
 import { createCustomAsyncThunk } from '@utils/store';
 import axios from 'axios';
 import { BaseReservation, Reservation } from './types';
 
-export const createReservation = createCustomAsyncThunk<void, BaseReservation>('reservations/create', {
+export const createReservation = createCustomAsyncThunk<void, BaseReservation[]>('reservations/create', {
     request: (payload) => axios.post('reservations/', payload),
     successMessage: 'Zarezerwowano wybrane sale',
     errorMessage: 'Wystąpił błąd podczas rezerwacji',
 });
 
-export const getReservation = createCustomAsyncThunk<void, Reservation, Reservation[]>('reservations/get', {
-    request: () => axios.get('reservations/'),
+export const getReservation = createCustomAsyncThunk<void, ListRequestPayload<Reservation>, Reservation[]>('reservations/get', {
+    request: (payload) => getList('reservations/', payload?.page, payload?.filters),
     errorMessage: 'Wystąpił błąd podczas pobierania rezerwacji',
 });
 
