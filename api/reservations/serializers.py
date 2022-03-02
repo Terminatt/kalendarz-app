7f9c610e97bfb4004de1ee75677d287afade0267
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-03-02 17:52:13 +0100

    feat(api): provide user for reservation

--- api/reservations/serializers.py
+++ api/reservations/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
+from users.serializers import UserSerializer
 from users.models import User
 from reservations.models import Reservation
 
 
 class ReservationSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(default=serializers.CurrentUserDefault(), queryset=User.objects.all())
+
+    def to_representation(self, obj):
+        self.fields['user'] = UserSerializer()
+        return super(ReservationSerializer, self).to_representation(obj)
     class Meta:
         model = Reservation
         fields = ['id', 'created', 'start', 'end', 'room', 'user']
