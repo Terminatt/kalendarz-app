9e16799d0dd75f55e29fd8329a6542438ffc4b38
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-03-07 17:34:23 +0100

    feat(front): clear summary after changing day

--- api/constants.py
+++ api/constants.py
@@ -4,4 +4,4 @@ EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 PASSWORD_MIN_LENGHT = 9
 GROUPS = ['administrator', 'regular_user']
 TOKEN_EXPIRATION = 24 # In Hours
-COOKIE_SAME_SITE = None if DEBUG else 'lax'
+COOKIE_SAME_SITE = None if DEBUG else 'lax' 
