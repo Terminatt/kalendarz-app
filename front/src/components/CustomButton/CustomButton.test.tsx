91d7759e2e670a7f6a7ed9b42d29412d05f38b3b
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-03-10 17:09:04 +0100

    testing(front): add custom button tests

---
+++ front/src/components/CustomButton/CustomButton.test.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import CustomButton from './CustomButton';
+import { MenuOutlined } from '@ant-design/icons';
+
+describe('Custom Button Component Variants', () => {
+    it('Primary matches the snapshot', () => {
+        const tree = renderer
+            .create(
+                <CustomButton variant="primary">Test</CustomButton>
+            )
+            .toJSON();
+        
+            expect(tree).toMatchSnapshot();
+    });
+
+    it('Default matches the snapshot', () => {
+        const tree = renderer
+            .create(
+                <CustomButton variant="default">Test</CustomButton>
+            )
+            .toJSON();
+        
+            expect(tree).toMatchSnapshot();
+    });
+
+    it('Delete matches the snapshot', () => {
+        const tree = renderer
+            .create(
+                <CustomButton variant="delete">Test</CustomButton>
+            )
+            .toJSON();
+        
+            expect(tree).toMatchSnapshot();
+    });
+
+    it('Clear matches the snapshot', () => {
+        const tree = renderer
+            .create(
+                <CustomButton variant="clear">Test</CustomButton>
+            )
+            .toJSON();
+        
+            expect(tree).toMatchSnapshot();
+    });
+
+    it('Icon matches the snapshot', () => {
+        const tree = renderer
+            .create(
+                <CustomButton variant="icon"><MenuOutlined /></CustomButton>
+            )
+            .toJSON();
+        
+            expect(tree).toMatchSnapshot();
+    });
+
+
+    it('Icon Border matches the snapshot', () => {
+        const tree = renderer
+            .create(
+                <CustomButton variant="icon-border"><MenuOutlined /></CustomButton>
+            )
+            .toJSON();
+        
+            expect(tree).toMatchSnapshot();
+    });
+
+    it('Minimal matches the snapshot', () => {
+        const tree = renderer
+            .create(
+                <CustomButton variant="minimal">Test</CustomButton>
+            )
+            .toJSON();
+        
+            expect(tree).toMatchSnapshot();
+    });
+});
