11787fce93c653e33e12d1a713d1ba129ae1102d
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-03-21 19:41:39 +0100

    feat(front): add handlers to action buttons

--- front/src/pages/UserZone/MyReservations/MyReservations.tsx
+++ front/src/pages/UserZone/MyReservations/MyReservations.tsx
@@ -1,11 +1,14 @@
 import React, { useCallback, useEffect } from 'react';
 import SingleColumnLayout from '@components/SingleColumnLayout/SingleColumnLayout';
 import { useDispatch, useSelector } from 'react-redux';
-import { getReservations } from '@store/reservations/asyncActions';
+import { deleteReservation, getReservations } from '@store/reservations/asyncActions';
 import CustomList from '@components/CustomList/CustomList';
 import { RootState } from '@store/index';
 import CustomButton from '@components/CustomButton/CustomButton';
 import { FullDataReservation } from '@store/reservations/types';
+import { useNavigate } from 'react-router';
+import { parseIsoDate } from '@utils/general';
+import DeletePopconfirm from '@components/DeletePopconfirm/DeletePopconfirm';
 import { MyReservationsItem } from './MyReservationsItem/MyReservationsItem';
 
 import './MyReservations.less';
@@ -14,19 +17,40 @@ const MyReservations: React.FC = () => {
     const dispatch = useDispatch();
     const { data, isLoading } = useSelector((state: RootState) => state.reservation);
     const { currentUser } = useSelector((state: RootState) => state.user);
+    const navigate = useNavigate();
+
+    const getMyReservations = useCallback(() => {
+        dispatch(getReservations({ requestPayload: { filters: { user: currentUser?.id } } }));
+    }, [currentUser?.id]);
 
     useEffect(() => {
         if (!currentUser) {
             return;
         }
 
-        dispatch(getReservations({ requestPayload: { filters: { user: currentUser?.id } } }));
+        getMyReservations();
     }, [currentUser?.id]);
 
+    const goToDay = useCallback((item: FullDataReservation) => {
+        navigate(`/room-reservation?day=${parseIsoDate(item.start)}`);
+    }, []);
+
+    const deleteMyReservation = useCallback((item: FullDataReservation) => {
+        dispatch(deleteReservation({
+            requestPayload: item.id,
+            onSuccess: () => {
+                getMyReservations();
+            },
+        }));
+    }, []);
+
     const renderActionButtons = useCallback((item: FullDataReservation) => [
-        <CustomButton size="small" key="goto">Przejdź do dnia</CustomButton>,
-        <CustomButton size="small" variant="delete" key="unschedule">Odwołaj</CustomButton>,
+        <CustomButton size="small" key="goto" onClick={() => goToDay(item)}>Przejdź do dnia</CustomButton>,
+        <DeletePopconfirm onConfirm={() => deleteMyReservation(item)} key="unschedule" title="Czy na pewno chcesz odwołać tą rezerwacje?">
+            <CustomButton size="small" variant="delete">Odwołaj</CustomButton>
+        </DeletePopconfirm>,
     ], []);
+
     return (
         <div className="my-reservations">
             <SingleColumnLayout className="my-reservations-content">
