7d1b720da608d4e228e22235bce3791908a2c944
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-03-27 14:21:58 +0200

    feat(front): add axios interceptor for 401 error

--- front/src/axios/axios.ts
+++ front/src/axios/axios.ts
@@ -1,6 +1,16 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
-export default axios.create({
+const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8000/' : undefined,
     withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(undefined, (error: AxiosError) => {
+    if (error.code === '401') {
+        window.location.replace('/');
+    }
+
+    return Promise.reject(error);
+});
+
+export default axiosInstance;
