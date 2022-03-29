316923e33360d34fe817f6eb2bf34b241d725740
Ernest Rudnicki <e.rudnicki96@gmail.com>
2022-03-29 20:37:28 +0200

    feat(api): add confirmed fiedl for reservation entity

--- api/reservations/models.py
+++ api/reservations/models.py
@@ -6,6 +6,7 @@ class Reservation(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     start = models.DateTimeField()
     end = models.DateTimeField()
+    confirmed = models.BooleanField(default=False)
     room = models.ForeignKey(Room, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     class Meta:
